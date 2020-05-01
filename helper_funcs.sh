c81ea97b0ae6539dd7b5064575a10e7102c68ffe
David Robinson <18422930+dsr085@users.noreply.github.com>
2020-04-30 20:22:32 -0500

    Update helper_funcs.sh

--- helper_funcs.sh
+++ helper_funcs.sh
@@ -2,7 +2,7 @@
 echo "Copy paste stuff you want out of this."
 exit 1
 
-#These show the helper hints in the PS1. Stick in .bashrc / etc. to be loaded.
+#These show the helper hints in the PS1. Stick in .bashrc / etc. to be loaded. Windows only. Can be slow in large repos.
 export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWSTASHSTATE=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
@@ -11,6 +11,10 @@ export GIT_PS1_SHOWUPSTREAM=1
 # Embrace the lazy
 cr() { command cd "/f/repos"/"$@"; }
 gp() { command git pull; }
+
+#Newer version of git allow you to configure fetch/pull to trim branches for you. Much better.
+#Also can alias git fetch -> git fetch --prune
+#git config remote.origin.prune true
 grpo() { command git remote prune origin; }
 
 # Deletes all non master local branch... this is dangerous. duh.
